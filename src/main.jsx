cdb66e6dbaa324b57ea63ad64fe2ffeca894fbfc
Prathap goud <65637706+pandugoud@users.noreply.github.com>
2026-04-21 16:47:42 +0530

    Replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <ToastContainer />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
-
